c47b01c879bd2ea4f0e3343fc358da114f011843
Luca Ferroni <luca@befair.it>
2015-12-21 16:29:43 +0100

    WARNING: aggiunto script che uso io per aggionrnare i file binari del package.zip
    non so perche' ma alla decompressione ho degli errori

---
+++ upgrade_bin_files.sh
@@ -0,0 +1,10 @@
+
+# During package.zip decompression I get error in some binary files
+
+cp ./www/lib/slick-carousel/slick/ajax-loader.gif ../fabrianodigital/ui/lib/slick-carousel/slick/
+cp -a ./www/test_data/* ../fabrianodigital/ui/test_data/
+cp -a ./www/img/fabriano/* ../fabrianodigital/ui/img/fabriano/
+cp ./www/lib/slick-carousel/slick/fonts/slick.woff ../fabrianodigital/ui/lib/slick-carousel/slick/fonts/
+cp ./www/lib/ionic/fonts/ionicons.woff ../fabrianodigital/ui/lib/ionic/fonts/
+cp ./www/lib/ionic/fonts/ionicons.eot ../fabrianodigital/ui/lib/ionic/fonts/
+
