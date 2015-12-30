1cd0b1e4c233d5a14d58df166de8ee133c13bf74
Luca Ferroni <luca@befair.it>
2015-12-31 00:24:55 +0100

    Aggiornato il mio script di compilazione app ionic

--- my_ionic_build.sh
+++ my_ionic_build.sh
@@ -8,10 +8,10 @@ cd -
 
 # During package.zip decompression I get error in some binary files
 
-cp ./www/lib/slick-carousel/slick/ajax-loader.gif ../fabrianodigital/ui/lib/slick-carousel/slick/
-cp -a ./www/test_data/* ../fabrianodigital/ui/test_data/
-cp -a ./www/img/fabriano/* ../fabrianodigital/ui/img/fabriano/
-cp ./www/lib/slick-carousel/slick/fonts/slick.woff ../fabrianodigital/ui/lib/slick-carousel/slick/fonts/
-cp ./www/lib/ionic/fonts/ionicons.woff ../fabrianodigital/ui/lib/ionic/fonts/
-cp ./www/lib/ionic/fonts/ionicons.eot ../fabrianodigital/ui/lib/ionic/fonts/
+cp -v ./www/lib/slick-carousel/slick/ajax-loader.gif ../fabrianodigital/ui/lib/slick-carousel/slick/
+cp -av ./www/test_data/* ../fabrianodigital/ui/test_data/
+cp -av ./www/img/* ../fabrianodigital/ui/img/
+cp -v ./www/lib/slick-carousel/slick/fonts/slick.woff ../fabrianodigital/ui/lib/slick-carousel/slick/fonts/
+cp -v ./www/lib/ionic/fonts/ionicons.woff ../fabrianodigital/ui/lib/ionic/fonts/
+cp -v ./www/lib/ionic/fonts/ionicons.eot ../fabrianodigital/ui/lib/ionic/fonts/
 
